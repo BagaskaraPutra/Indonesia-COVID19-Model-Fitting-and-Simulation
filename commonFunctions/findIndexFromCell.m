b2498e2c6885dee9c500d7550278dc99d9756c3f
BagaskaraPutra <primastyaputra@gmail.com>
2020-09-16 15:52:56 +0700

    Added new models: SIRQN & SEIQRDP

--- commonFunctions/findIndexFromCell.m
+++
@@ -1,6 +0,0 @@
-function indexCell = findIndexFromCell(sourceCell,desiredValue)
-  for i=1:numel(sourceCell)
-    if(sourceCell{i} == desiredValue)
-      indexCell = i;
-    end
-  end
--- commonFunctions/nan2zero.m
+++
@@ -1,8 +0,0 @@
-function zero = nan2zero(data)
-for i=1:length(data)
-    if(isnan(data(i)))
-        zero(i) = 0;
-    else
-        zero(i) = data(i);
-    end
-end
