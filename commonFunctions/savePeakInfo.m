43bbd38e6eb40b7e00e13db6537b5d962454402d
BagaskaraPutra <primastyaputra@gmail.com>
2020-09-19 18:00:03 +0700

    Renamed all peakInfo files and its function to prevent long file names

--- commonFunctions/savePeakInfo.m
+++ commonFunctions/savePeakInfo.m
@@ -1,7 +1,7 @@
 function savePeakInfo(k,saveDir)
 for i=1:numel(k)
     if(isfield(k{i},'peak'))
-        fid=fopen([saveDir '/' 'PuncakTerinfeksiKebijakan' num2str(i) '.' k{i}.name '.csv'],'w');
+        fid=fopen([saveDir '/' num2str(i) '.' k{i}.name '.csv'],'w');
            fprintf(fid,'Jumlah:, %.0f\n',k{i}.peak.y);
            fprintf(fid,'Tanggal:,');
            fprintf(fid,datestr(k{i}.timeSim{k{i}.peak.x}));
