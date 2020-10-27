6847ba05eccdead5ce435d913fab7446a6ffa3af
BagaskaraPutra <primastyaputra@gmail.com>
2020-10-27 21:59:44 +0700

    Even more dynamic loadModel.m with customizable header type

--- commonFunctions/loadModel.m
+++ commonFunctions/loadModel.m
@@ -1,9 +1,18 @@
 function model = loadModel(model)
 file = importdata([model.dir '/config.txt']); %[EDITABLE] if you change the config file name
+ignoreChar = '%'; %[EDITABLE] if you change the ignore or comment header type
 headerStartChar = '['; headerEndChar = ']:'; %[EDITABLE] if you change the config header format
 
-headerIndex = {};
-tempIndex = strfind(file,headerEndChar);
+% Find cells containing ignoreChar, and remove the strings after the ignoreChar, 
+% basically ignore the comment marked with ignoreChar
+ignoreIndex = strfind(file,ignoreChar);
+for igIdx=1:numel(ignoreIndex)
+    if(~isempty(ignoreIndex))
+        file{igIdx} = erase(file{igIdx},file{igIdx}(ignoreIndex{igIdx}:end));
+    end
+end
+
+headerIndex = {}; tempIndex = strfind(file,headerEndChar);
 for hIdxFound=1:numel(tempIndex)
     if(~isempty(tempIndex{hIdxFound}))
         headerIndex = [headerIndex hIdxFound];
@@ -11,23 +20,13 @@ for hIdxFound=1:numel(tempIndex)
 end
 
 for hIdx=1:numel(headerIndex)
-    if(hIdx == numel(headerIndex)) % last headerName
-        for fIdx=1:size(file,1)-headerIndex{hIdx}
-            headerName = extractCharBetween(file{headerIndex{hIdx}},headerStartChar,headerEndChar);
-            model.(headerName){fIdx} = file{fIdx+headerIndex{hIdx}};
-        end
-    else % middle headerNames
-        for fIdx=1:(headerIndex{hIdx+1}-headerIndex{hIdx})-1
-            headerName = extractCharBetween(file{headerIndex{hIdx}},headerStartChar,headerEndChar);
-            model.(headerName){fIdx} = file{fIdx+headerIndex{hIdx}}; 
+    headerName = extractCharBetween(file{headerIndex{hIdx}},headerStartChar,headerEndChar);
+    focusedHeaderFile = file(getHeaderContentIndex(file,headerStartChar,headerEndChar,headerName));
+    for fIdx=1:numel(focusedHeaderFile)
+        if(~isempty(focusedHeaderFile{fIdx}))
+            model.(headerName){fIdx} = focusedHeaderFile{fIdx};
         end
     end
-end
-
-function outputChar = extractCharBetween(inputChar,startChar,endChar)
-    startIdx = strfind(inputChar,startChar);
-    endIdx = strfind(inputChar,endChar);
-    outputChar = inputChar(startIdx+1:endIdx-1);
 end
 
 end
