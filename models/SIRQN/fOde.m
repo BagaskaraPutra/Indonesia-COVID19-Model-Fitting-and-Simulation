3f894a4a75d1350bec8208b5ae7767959e8d521b
BagaskaraPutra <primastyaputra@gmail.com>
2020-11-05 14:31:53 +0700

    Removed redundant models and files

--- models/SIRQN/fOde.m
+++ models/SIRQN/fOde.m
@@ -1,29 +1,26 @@
 % Function calculating the differential equations for the COVID-19 model 
 function dydt = fOde(t,y, param) 
 global Npop;
-dydt = zeros(8,1);
+dydt = zeros(7,1);
 S = y(1); 
 NQ = y(2); 
-NR = y(3); 
-ND = y(4);
-I = y(5);
-Q = y(6); 
-R = y(7); 
-D = y(8);
+ND = y(3);
+I = y(4);
+Q = y(5); 
+R = y(6); 
+D = y(7);
 
 beta = param(1); 
 r_S_NQ = param(2); 
-r_NR_S = param(3);
-r_NQ_NR = param(4);
-r_NQ_Q = param(5);
-r_NQ_ND = param(6);
-r_I_Q = param(7);
-gamma = param(8);
-muI = param(9);
+r_NQ_S = param(3);
+r_NQ_Q = param(4);
+r_NQ_ND = param(5);
+r_I_Q = param(6);
+gamma = param(7);
+muI = param(8);
 
-dSdt = -beta*S*I/Npop - r_S_NQ*S + r_NR_S*NR;
-dNQdt = r_S_NQ*S - r_NQ_NR*NQ - r_NQ_ND*NQ - r_NQ_Q*NQ;
-dNRdt = -r_NR_S*NR + r_NQ_NR*NQ;
+dSdt = -beta*S*I/Npop - r_S_NQ*S + r_NQ_S*NQ;
+dNQdt = r_S_NQ*S - r_NQ_S*NQ - r_NQ_ND*NQ - r_NQ_Q*NQ;
 dNDdt = r_NQ_ND*NQ;
 dIdt = beta*S*I/Npop - r_I_Q*I;
 dQdt = r_NQ_Q*NQ + r_I_Q*I - gamma*Q - muI*Q;
@@ -32,9 +29,8 @@ dDdt = muI*Q;
 
 dydt(1) = dSdt;  
 dydt(2) = dNQdt; 
-dydt(3) = dNRdt; 
-dydt(4) = dNDdt;
-dydt(5) = dIdt;
-dydt(6) = dQdt; 
-dydt(7) = dRdt; 
-dydt(8) = dDdt;
+dydt(3) = dNDdt;
+dydt(4) = dIdt;
+dydt(5) = dQdt; 
+dydt(6) = dRdt; 
+dydt(7) = dDdt;
