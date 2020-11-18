5a934b8b348dc5d6b10d43ba940c0ab2140d486e
BagaskaraPutra <primastyaputra@gmail.com>
2020-11-18 09:50:03 +0700

    Indonesia data update

--- commonFunctions/LHS-PRCC/PRCC.m
+++ commonFunctions/LHS-PRCC/PRCC.m
@@ -8,16 +8,19 @@
 % by Simeone Marino, May 29 2007 
 % modified by Marissa Renardy, October 8 2020
 % N.B.: this version uses ONE output at a time
+% modified by Bagaskara P P, November 17 2020
+% plot sorted parameters from most to least significant 
 
-function [prcc,sign,sign_label]=PRCC(LHSmatrix,Y,s,PRCC_var,alpha);
-
+function [prcc,sign,sign_label]=PRCC(LHSmatrix,model,time_points,PRCC_var,alpha);
+s = 1:length(time_points); % index for time points
+Y = model.state.(model.analyzeThisOutput).lhs; % desired analyzed output LHS
 Y=Y(s,:)';% Define the output. Comment out if the Y is already 
           % a subset of all the time points and it already comprises
           % ONLY the s rows of interest
 [a,k]=size(LHSmatrix); % Define the size of LHS matrix
 [b,out]=size(Y);
 for i=1:k  % Loop for the whole submatrices
-    c=['LHStemp=LHSmatrix;LHStemp(:,',num2str(i),')=[];Z',num2str(i),'=LHStemp;LHStemp=[];'];
+    c=['LHStemp=LHSmatrix; LHStemp(:,',num2str(i),')=[]; Z',num2str(i),'=LHStemp; LHStemp=[];'];
     eval(c);
     % Loop to calculate PRCCs and significances
     c1=['[LHSmatrix(:,',num2str(i),'),Y];'];
@@ -53,26 +56,47 @@ sign=uncorrected_sign;
 %uncorrected_sign; % uncorrected p-value
 %Bonf_sign;  % Bonferroni correction
 
-sign_label_struct=struct;
-sign_label_struct.uncorrected_sign=uncorrected_sign;
+sign_label=struct;
+sign_label.uncorrected_sign=uncorrected_sign;
 
 %figure
 for r=1:length(s)
     figure();
-    c1=['PRCCs at time = ' num2str(s(r))];
+%     c1=['PRCCs at time = ' num2str(s(r))];
+    c1=['PRCCs at time point: ' num2str(time_points(r))];
     a=find(uncorrected_sign(r,:)<alpha);
-    ['Significant PRCCs'];
-    a;
     PRCC_var(a);
     prcc(r,a);
     b=num2str(prcc(r,a));
-    sign_label_struct.index{r}=a;
-    sign_label_struct.label{r}=PRCC_var(a);
-    sign_label_struct.value{r}=b;
+    sign_label.index{r}=a;
+    sign_label.label{r}=PRCC_var(a);
+    sign_label.value{r}=b;
     %% Plots of PRCCs and their p-values
     subplot(1,2,1),bar(PRCCs(r,:)),title(c1);
         set(gca,'XTickLabel',PRCC_var,'XTick',[1:k]); %,title('PRCCs');
     subplot(1,2,2),bar(uncorrected_sign(r,:)),...
        set(gca,'YLim',[0,.1]),title('P values');%'XTickLabel',PRCC_var,'XTick',[1:k],
+   %% Plot sorted PRCCs
+   % print all prcc parameters sorted from most to least significant
+    [absPrccSortedValue, prccSortedIdx] = sort(abs(prcc(r,:)),'descend'); 
+    fprintf('%d. TimePoint: %d\n',r,time_points(r));
+    for sortIdx=1:numel(prccSortedIdx)
+        prccSortedValue(sortIdx) = prcc(r,prccSortedIdx(sortIdx));
+        prccSortedParamName{sortIdx} = model.paramName{prccSortedIdx(sortIdx)};
+        prccSorted_var{sortIdx} = PRCC_var{prccSortedIdx(sortIdx)};
+        fprintf('%s: %f',prccSortedParamName{sortIdx}, prccSortedValue(sortIdx));
+        for signIdx=1:numel(sign_label.index{r})
+            if(prccSortedIdx(sortIdx) == sign_label.index{r}(signIdx))
+                fprintf(' (significant)');
+            end
+        end
+        fprintf('\n');
+    end
+    fprintf('\n');
+
+   figure();
+    bar(prccSortedValue); title(['Most to Least Significant ' c1]);
+        set(gca,'XTickLabel',prccSorted_var,'XTick',[1:k]);
 end
-sign_label=sign_label_struct;
+
+end
